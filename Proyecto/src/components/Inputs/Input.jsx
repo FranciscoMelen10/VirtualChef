cdf7dd83433cfd7f4c2fc6c7c003adcb791e7e39
Ed Centeno <cchavarriacenteno8@gmail.com>
2024-03-04 00:56:43 -0600

    Update: features extra a los inputs
    Ahora los inputs pueden recibir parametros que definiran si son multilinea, su maximo de caracteres o maximo de lineas, etc

--- Proyecto/src/components/Inputs/Input.jsx
+++ Proyecto/src/components/Inputs/Input.jsx
@@ -1,38 +1,46 @@
 import React from 'react';
-import { StyleSheet, TextInput, View, Text } from 'react-native';
+import {StyleSheet, TextInput, View, Text} from 'react-native';
 
-const Input = ({ name }) => {
-    return (
-        <View style={styles.container}>
-            <TextInput
-                style={styles.input}
-            />
-            <Text style={styles.text_location}>{name}</Text>
-        </View>
-    )
-}
+const Input = ({
+  name,
+  isMultiline = false,
+  numberOfLines = 1,
+  maxLength = 20,
+}) => {
+  return (
+    <View style={styles.container}>
+      <TextInput
+        editable
+        multiline={isMultiline}
+        numberOfLines={numberOfLines}
+        maxLength={maxLength}
+        style={styles.input}
+      />
+      <Text style={styles.text_location}>{name}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        position: 'relative',
-    },
-    text_location: {
-        position: 'absolute',
-        top: 2,
-        left: 22,
-        backgroundColor: '#fff',
-        zIndex: 1, // Asegura que el Text esté encima del TextInput
-        paddingHorizontal:3
-    },
-    input: {
-        height: 40,
-        width: 270,
-        margin: 12,
-        borderWidth: 1,
-        padding: 10,
-        borderRadius: 5,
-        zIndex: 0, // Asegura que el TextInput esté detrás del Text
-    },
+  container: {
+    position: 'relative',
+  },
+  text_location: {
+    position: 'absolute',
+    top: 2,
+    left: 22,
+    backgroundColor: '#fff',
+    zIndex: 1, // Asegura que el Text esté encima del TextInput
+    paddingHorizontal: 3,
+  },
+  input: {
+    width: 320,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+    zIndex: 0, // Asegura que el TextInput esté detrás del Text
+  },
 });
 
 export default Input;
