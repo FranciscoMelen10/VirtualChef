ed504f07f7b989612968c14898621f63acd3bbc4
Ed Centeno <cchavarriacenteno8@gmail.com>
2024-03-03 21:05:06 -0600

    Add: Nuevos iconos para el popup menu

--- Proyecto/src/components/Icon/constante-svg.jsx
+++ Proyecto/src/components/Icon/constante-svg.jsx
@@ -3,15 +3,17 @@
     de screens para las aplicaciones de React native
 */
 
-import Logo from '../../../assets/Logo.svg'
-import Buscar from '../../../assets/Buscar.svg'
-import Reloj from '../../../assets/Reloj.svg'
-import Corazon from '../../../assets/Corazon.svg'
+import Logo from '../../../assets/Logo.svg';
+import Buscar from '../../../assets/Buscar.svg';
+import Reloj from '../../../assets/Reloj.svg';
+import Corazon from '../../../assets/Corazon.svg';
+import PopUpMenuIcon from '../../../assets/popUpMenuIcon.svg';
 
 export const Iconos = {
-    LogoXL: <Logo width={200} height={200} />,
-    Logo: <Logo width={20} height={20} />,
-    Buscar: <Buscar width={20} height={20} />,
-    Reloj: <Reloj width={12} height={12} />,
-    Corazon: <Corazon width={20} height={20} />,
-  };
+  LogoXL: <Logo width={200} height={200} />,
+  Logo: <Logo width={20} height={20} />,
+  Buscar: <Buscar width={20} height={20} />,
+  Reloj: <Reloj width={12} height={12} />,
+  Corazon: <Corazon width={25} height={25} />,
+  PopUpMenuIcon: <PopUpMenuIcon width={25} height={25} />,
+};
