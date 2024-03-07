a74b0dfdd23a81fe43fab42ae90bee21e1a9fcdd
FranciscoMelen10 <francisco.melendez14454@est.uca.edu.ni>
2024-03-06 22:16:13 -0600

    Update: Aplicando buenas practicas al componente de comidas y principal

--- Proyecto/src/components/Carrusel/CarruselComidas.jsx
+++ Proyecto/src/components/Carrusel/CarruselComidas.jsx
@@ -1,105 +1,38 @@
-// import Corazon from "../../assets/Corazon.svg";
-import React from "react";
+import React from 'react';
 import {
   StyleSheet,
   Text,
   View,
-  Dimensions,
-  SafeAreaView,
   ScrollView,
-} from "react-native";
+} from 'react-native';
 
-import CardComidas from "../Cards/CardComida";
-import Ejemplo from "../../../assets/Ejemplo.png";
-import Ejemplo2 from "../../../assets/Ejemplo2.png";
-import Ejemplo3 from "../../../assets/Ejemplo3.png";
+import CardComidas from '../Cards/CardComida';
 
-const WIDTH_WINDOW = Dimensions.get("window").width;
-// const HEIGHT_WINDOW = Dimensions.get("window").height;
-
-const FOODS_CONST = [
-  {
-    name: "Pollo frito",
-    time: "10",
-    img: Ejemplo,
-  },
-  {
-    name: "Pescado frito",
-    time: "10",
-    img: Ejemplo2,
-  },
-  {
-    name: "Ensalada",
-    time: "10",
-    img: Ejemplo3,
-  },
-];
-
-export default function CarruselComida() {
+export default function CarruselComida({horario, datos}) {
   return (
-    <SafeAreaView style={styles.principal}>
-      <ScrollView>
-        {/* Desayuno */}
-        <View style={styles.container}>
-          <Text style={styles.text}>Desayuno</Text>
-          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-            {FOODS_CONST.map((data, index) => {
-              return (
-                <CardComidas
-                  imagen={data.img}
-                  name={data.name}
-                  time={data.time}
-                  key={data.name + index}
-                ></CardComidas>
-              );
-            })}
-          </ScrollView>
-        </View>
-
-        <View style={styles.container}>
-          <Text style={styles.text}>Almuerzo</Text>
-          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-            {FOODS_CONST.map((data, index) => {
-              return (
-                <CardComidas
-                  imagen={data.img}
-                  name={data.name}
-                  time={data.time}
-                  key={data.name + index}
-                ></CardComidas>
-              );
-            })}
-          </ScrollView>
-        </View>
-
-        <View style={styles.container}>
-          <Text style={styles.text}>Cena</Text>
-          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-            {FOODS_CONST.map((data, index) => {
-              return (
-                <CardComidas
-                  imagen={data.img}
-                  name={data.name}
-                  time={data.time}
-                  key={data.name + index}
-                ></CardComidas>
-              );
-            })}
-          </ScrollView>
-        </View>
+    <View style={styles.container}>
+      <Text style={styles.text}>{horario}</Text>
+      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+        {datos.map((data, index) => {
+          return (
+            <CardComidas
+              imagen={data.img}
+              name={data.name}
+              time={data.time}
+              key={data.name + index}
+            ></CardComidas>
+          );
+        })}
       </ScrollView>
-    </SafeAreaView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  principal: {
-    flex: 1,
-    width: WIDTH_WINDOW,
-  },
+
   text: {
     fontSize: 30,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   container: {
     margin: 20,
--- Proyecto/src/pages/Home/Principal.jsx
+++ Proyecto/src/pages/Home/Principal.jsx
@@ -1,18 +1,59 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
-import CarruselComida from "../../components/Carrusel/CarruselComidas";
-import { SafeAreaView } from "react-native-safe-area-context";
-import InputIcon from "../../components/Inputs/InputIcon";
-import { Iconos } from "../../components/Icon/constante-svg";
+import React from 'react';
+import {StyleSheet, ScrollView, Dimensions, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+import CarruselComida from '../../components/Carrusel/CarruselComidas';
+import InputIcon from '../../components/Inputs/InputIcon';
+
+import {Iconos} from '../../components/Icon/constante-svg';
+import Ejemplo from '../../../assets/Ejemplo.png';
+import Ejemplo2 from '../../../assets/Ejemplo2.png';
+import Ejemplo3 from '../../../assets/Ejemplo3.png';
+
+const FOODS_CONST = [
+  {
+    name: 'Pollo frito',
+    time: '10',
+    img: Ejemplo,
+  },
+  {
+    name: 'Pescado frito',
+    time: '10',
+    img: Ejemplo2,
+  },
+  {
+    name: 'Ensalada',
+    time: '10',
+    img: Ejemplo3,
+  },
+];
+
+const WIDTH_WINDOW = Dimensions.get('window').width;
 
 const Home = () => {
   return (
     <SafeAreaView style={styles.container}>
       <InputIcon
-        placeholder={"Buscar recetas..."}
+        placeholder={'Buscar recetas...'}
         icono={Iconos.Buscar}
       ></InputIcon>
-      <CarruselComida></CarruselComida>
+
+      <View style={styles.principal}>
+        <ScrollView>
+          <CarruselComida
+            datos={FOODS_CONST}
+            horario={'Desayuno'}
+          ></CarruselComida>
+          <CarruselComida
+            datos={FOODS_CONST}
+            horario={'Almuerzo'}
+          ></CarruselComida>
+          <CarruselComida
+            datos={FOODS_CONST}
+            horario={'Desayuno'}
+          ></CarruselComida>
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 };
@@ -20,9 +61,13 @@ const Home = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 10,
+  },
+  principal: {
+    flex: 1,
+    width: WIDTH_WINDOW,
   },
 });
 
